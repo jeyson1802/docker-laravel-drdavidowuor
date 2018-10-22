c17aaea6b8b076da5ec9f0130bcae87fa9d6bee3
Jeyson Ventura Aguilar <venturajj@globalhitss.com>
2018-10-22 01:16:42 -0500

    anace de listado

--- resources/views/listarcruzada.blade.php
+++ resources/views/listarcruzada.blade.php
@@ -18,54 +18,55 @@
                             <a href="#" class="btn btn-primary" >Exportar - {{ $cruzadas->count() }} <span class="glyphicon glyphicon-save"></span></a>
                             </div>
                         </div>
-                    </div>
-                    <div class="table-responsive">
-                        <table id="mytable" class="table table-bordred table-striped">
-                        <thead>
-                        <th>Nombres</th>
-                        <th>Apellidos</th>
-                        <th>Correo</th>
-                        <th>Celular</th>
-                        <th>Facebook</th>
-                        <th>País</th>
-                        <th>Iglesia</th>
-                        <th>Oración</th>
-                        <th>Noticias</th>
-                        <th>Eliminar</th>
-                        </thead>
-                        <tbody>
-                        @if($cruzadas->count())  
-                        @foreach($cruzadas as $cruzada)  
-                        <tr>
-                            <td>{{$cruzada->nombres}}</td>
-                            <td>{{$cruzada->apellidos}}</td>
-                            <td>{{$cruzada->correo}}</td>
-                            <td>{{$cruzada->celular}}</td>
-                            <td>{{$cruzada->facebook}}</td>
-                            <td>{{$cruzada->pais}}</td>
-                            <td>{{$cruzada->iglesia}}</td>
-                            <td>{{$cruzada->oracion}}</td>
-                            <td>{{$cruzada->noticias}}</td>  
-                            <td>                          
-                            <form action="{{action('CruzadaController@destroy', $cruzada->id)}}" method="post">
-                            {{csrf_field()}}
-                            <input name="_method" type="hidden" value="DELETE">
-                            <button class="btn btn-danger btn-xs" type="submit"><span class="glyphicon glyphicon-trash"></span></button>
-                            </td>
-                        </tr>
-                        @endforeach 
-                        @else
-                        <tr>
-                            <td colspan="8">No hay registro !!</td>
-                        </tr>
-                        @endif
-                        </tbody>
+                        <div class="table-responsive">
+                            <table id="mytable" class="table table-bordred table-striped">
+                                <thead>
+                                <th>Nombres</th>
+                                <th>Apellidos</th>
+                                <th>Correo</th>
+                                <th>Celular</th>
+                                <th>Facebook</th>
+                                <th>País</th>
+                                <th>Iglesia</th>
+                                <th>Oración</th>
+                                <th>Noticias</th>
+                                <th>Eliminar</th>
+                                </thead>
+                                <tbody>
+                                @if($cruzadas->count())  
+                                @foreach($cruzadas as $cruzada)  
+                                <tr>
+                                    <td>{{$cruzada->nombres}}</td>
+                                    <td>{{$cruzada->apellidos}}</td>
+                                    <td>{{$cruzada->correo}}</td>
+                                    <td>{{$cruzada->celular}}</td>
+                                    <td>{{$cruzada->facebook}}</td>
+                                    <td>{{$cruzada->pais}}</td>
+                                    <td>{{$cruzada->iglesia}}</td>
+                                    <td>{{$cruzada->oracion}}</td>
+                                    <td>{{$cruzada->noticias}}</td>  
+                                    <td>                          
+                                    <form action="{{action('CruzadaController@destroy', $cruzada->id)}}" method="post">
+                                    {{csrf_field()}}
+                                    <input name="_method" type="hidden" value="DELETE">
+                                    <button class="btn btn-danger btn-xs" type="submit"><span class="glyphicon glyphicon-trash"></span></button>
+                                    </td>
+                                </tr>
+                                @endforeach 
+                                @else
+                                <tr>
+                                    <td colspan="8">No hay registro !!</td>
+                                </tr>
+                                @endif
+                                </tbody>
 
-                    </table>
+                            </table>
+                        </div>
+                    </div>
                     </div>
                 </div>
                 {{ $cruzadas->links() }}
-    </section><!-- Main container end -->
+        </section><!-- Main container end -->
     </div>
 
 @endsection
